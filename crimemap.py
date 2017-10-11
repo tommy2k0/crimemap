cbd3a8b0100dc7824309197b47f85b2cb247ce0e
Tommy <tommy@yahoo.com>
2017-10-11 22:39:15 +0200

    Map with form and css

--- crimemap.py
+++ crimemap.py
@@ -20,21 +20,14 @@ def home():
         data = None
     return render_template("home.html", data=data)
 
-@app.route("/add", methods=["POST"])
-def add():
-    try:
-        data = request.form.get("userinput")
-        DB.add_input(data)
-    except Exception as e:
-        print(e)
-    return home()
-
-@app.route("/clear")
-def clear():
-    try:
-        DB.clear_all()
-    except Exception as e:
-        print(e)
+@app.route("/submitcrime", methods=["POST"])
+def submitcrime():
+    category = request.form.get("category")
+    date = request.form.get("date")
+    latitude = float(request.form.get("latitude"))
+    longitude = float(request.form.get("longitude"))
+    description = request.form.get("description")
+    DB.add_crime(category, date, latitude, longitude, description)
     return home()
 
 if __name__ == "__main__":
